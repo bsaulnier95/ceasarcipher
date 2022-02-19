0d0e5141c60227b0dde7670d4c1eca9939dcc290
Ben <you@example.com>
2022-02-19 16:42:03 -0500

    test

--- cipher.rb
+++ cipher.rb
@@ -5,11 +5,11 @@ def caesar_cipher(string, shift = 1)
     alphabet = Array('A'..'Z')
     caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
     
-    encrypter = non_caps.merge(caps)
+    encryption = non_caps.merge(caps)
     
-    string.chars.map { |c| encrypter.fetch(c, c) }
+    string.chars.map { |c| encryption.fetch(c, c) }
   end
   
   p caesar_cipher("testingzZ1Z").join
-  @matugm
+  
   
