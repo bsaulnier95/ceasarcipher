6e0d886a109a2166a581a8bd468a6c2870d728f8
Benjamin Saulnier <bsaulnier@cmmc02zx06dlvdg.innova.local>
2022-04-05 10:12:19 -0400

    Version 2

--- cipher.rb
+++ cipher.rb
@@ -1,9 +1,7 @@
+def caesar_cipher(string, shift = 1)
+  alphabet   = Array('a'..'z')
+  encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  string.chars.map { |c| encrypter.fetch(c, " ") }
+end
 
-  def cipher(string,key)
-    namem=[]
-    string.downcase.bytes.map do|c|
-    namem.push(c+key<123?(c+key):(c+key-26))
-    end
-    namem.pack('c*')
-    end
-    puts cipher('What a string!',5)
+p caesar_cipher("Hello World").join
